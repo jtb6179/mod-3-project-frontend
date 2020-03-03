06a50fe60b990019f6e1e6cf68b1997720e6d10a
JoelBly <jtb6179@g.rit.edu>
2020-03-03 11:54:10 -0500

    all most done man

--- src/index.js
+++ src/index.js
@@ -12,43 +12,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
         function renderPost(postObj) {
             let fillInBody = document.querySelector('body')
-            let theNavBar = document.createElement('nav')
+            let theNavBar = document.querySelector('#the-nav')
+            let specificSpan = document.querySelector('.specific-post')
+            let btnDiv = document.createElement('div')
             let theBreak = document.createElement('hr')
-            let sideBar = document.createElement('section')
+            let loginUser = document.createElement('form')
             let h1Above = document.createElement('h1')
-            let specificSpan = document.createElement('span')
             let anotherBreak = document.createElement('hr')
             let likeBtn = document.createElement('button')
-            let dislikeBtn = document.createElement('button')
-            let aMain = document.createElement('main')
+            // let dislikeBtn = document.createElement('button')
             let likeSum = document.createElement('div')
-            let dislikeSum = document.createElement('div')
+            // let dislikeSum = document.createElement('div')
             let lastBreak = document.createElement('hr')
             let allPostDiv = document.createElement('div')
             let postsSpan = document.createElement('span')
 
-            theNavBar.setAttribute('class', 'fh5co-nav')
-            theNavBar.setAttribute('id', 'trash-heap')
-            sideBar.setAttribute('id', `trash-post-number-${postObj.id}`)
-            sideBar.setAttribute('class', `offcanvas`)
-            likeBtn.setAttribute('class', `nice-button`)
-            dislikeBtn.setAttribute('class', `nice-button`)
+            // theNavBar.setAttribute('class', 'nav')
+            // theNavBar.setAttribute('id', 'trash-heap')
+            loginUser.setAttribute('label', `Username`)
+    //         <label for="uname"><b>Username</b></label>
+    // <input type="text" placeholder="Enter Username" name="uname" required></input>
+            // sideBar.setAttribute('class', `offcanvas`)
+            likeBtn.setAttribute('class', `btn`)
+            
+            // dislikeBtn.setAttribute('class', `btn`)
             allPostDiv.setAttribute('class', 'all-the-dump-drops')
             postsSpan.setAttribute('class', 'child-Span-list')
             
             h1Above.innerText = `Headline: ${postObj.title}`
-            // specificSpan.innerText = postObj.post_text
+            
             likeBtn.innerText = `Like! 👍`
-            dislikeBtn.innerText = `Dislike! 👎`
-            // likeSum.innerText = `likes: ${postObj.likes.count}`
-            // dislikeSum.innerText = `likes: ${postObj.likes.count}`
+            // theNavBar.
+            // dislikeBtn.innerText = `Dislike! 👎`
+            likeSum.innerText = `likes: 0`
+            // dislikeSum.innerText = `dislikes: 0`
             postsSpan.innerText = postObj.post_text
 
-            aMain.append(likeSum, dislikeSum)
-            specificSpan.append(likeBtn, dislikeBtn, aMain)
-            sideBar.append(specificSpan, anotherBreak)
-            allPostDiv.append(postsSpan)
-            fillInBody.append(sideBar, allPostDiv)
+            
+            // specificSpan.append( )
+            // sideBar.append( )
+            btnDiv.append(likeBtn)
+            theNavBar.appendChild(loginUser)
+            allPostDiv.append(postsSpan, btnDiv, anotherBreak, likeSum, theBreak)
+            fillInBody.append(allPostDiv)
+
+
 
      /* for event listener
      <span>
